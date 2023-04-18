2c2214c41dd3b5cf8d0bb54503d2f9c96751fdfe
Wijkarn <89905143+Wijkarn@users.noreply.github.com>
2023-04-18 15:06:05 +0200

    Add files via upload

---
+++ src/module/firebase.js
@@ -0,0 +1,37 @@
+export class fireBase {
+
+    highScoreArray;
+
+    constructor(highScoreArray) {
+        this.highScoreArray = highScoreArray;
+    }
+
+    // GET Highscore array from database
+    async getFirebase() {
+        const url = "https://rockpaperscissors-50a62-default-rtdb.europe-west1.firebasedatabase.app/highScore.json";
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        return data;
+    }
+
+    // Update database
+    async putToFireBase() {
+        // const arr = [{ name: "Wijk", score: 10 }, { name: "Max", score: 3 }, { name: "Markus", score: 5 }, { name: "Deez", score: 6 }, { name: "Nutz", score: 9 }];
+
+        const url = "https://rockpaperscissors-50a62-default-rtdb.europe-west1.firebasedatabase.app/highScore.json";
+
+        const options = {
+            method: "PUT",
+            body: JSON.stringify(highScoreArray),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        }
+
+        const response = await fetch(url, options);
+        const data = await response.json();
+        return data;
+    }
+}
